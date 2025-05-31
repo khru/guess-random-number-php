785f66f4d1c8d6cc07c4d0a8b9b11180e5bcb845
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2025-05-31 16:53:54 +0200

    green: the_user_will_give_a_lower_number

--- php/src/GuessRandomNumber.php
+++ php/src/GuessRandomNumber.php
@@ -5,12 +5,18 @@ namespace Application;
 class GuessRandomNumber
 {
 
+    private int $randomNumber;
+
     public function __construct(RandomNumberGenerator $generator)
     {
+        $this->randomNumber = $generator->generate();
     }
 
     public function play(int $guessAttempt): string
     {
+        if ($guessAttempt < $this->randomNumber) {
+            return 'higher';
+        }
         return 'won';
     }
 }
