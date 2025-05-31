9f973a43f7cdc55e8479471fafe8202a4e7a33ea
fdelcaz <fernando.caz@volkswagen-groupservices.com>
2025-05-31 17:13:57 +0200

    green: the_user_will_fail_three_times

--- php/src/GuessRandomNumber.php
+++ php/src/GuessRandomNumber.php
@@ -5,7 +5,9 @@ namespace Application;
 class GuessRandomNumber
 {
 
+    const MAX_ATTEMPTS = 3;
     private int $randomNumber;
+    private int $currentAttempts=0;
 
     public function __construct(RandomNumberGenerator $generator)
     {
@@ -14,6 +16,12 @@ class GuessRandomNumber
 
     public function play(int $guessAttempt): string
     {
+        $this->currentAttempts++;
+
+        if($this->currentAttempts >= GuessRandomNumber::MAX_ATTEMPTS){
+            return 'game over';
+        }
+
         if ($guessAttempt < $this->randomNumber) {
             return 'higher';
         }
